241653e0546e5cb8d92aa2c3995fccf7e619662b
taieb <belaid.taieb@gmail.com>
2023-09-05 12:33:44 +0200

    home page

--- Program.cs
+++ Program.cs
@@ -3,13 +3,12 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 app.UseStaticFiles();
 app.UseRouting();
-app.UseAuthentication();
+app.UseAuthorization();
 
 
 
 app.MapControllerRoute(
     name : "default",
-    pattern : "{controller=Home}/{action=Index}/{id?}"
-);
+    pattern : "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
