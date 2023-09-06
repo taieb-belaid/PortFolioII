2b585dcda049c4bd4d9e0e7857a9065f760bb333
taieb <belaid.taieb@gmail.com>
2023-09-06 10:47:09 +0200

    the contact view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,4 +8,15 @@ public class HomeController : Controller
     public ViewResult Index(){
         return View("Index");
     }
-}
+
+    [HttpGet]
+    [Route("projects")]
+    public ViewResult Projects (){
+        return View("Projects");
+    }
+
+    [HttpGet("contact")]
+    public ViewResult Contact(){
+        return View("Contact");
+    }
+}  
